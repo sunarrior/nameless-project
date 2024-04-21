ae092274259eb40aa181b16cd34e09a9b6b467ba
NhatHuynh <nhatthuynhh621@gmail.com>
2024-04-21 11:40:36 +0700

    chore: initial commit

---
+++ jest.config.ts
@@ -0,0 +1,11 @@
+/* eslint-disable */
+export default {
+  displayName: 'nest-monorepo-skeleton',
+  preset: './jest.preset.js',
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: './coverage/nest-monorepo-skeleton',
+};
